6d9ad6716eef785ccecd646facc670b4786d3adc
azighaazhar <azighaazhar@student.telkomuniversity.ac.id>
2025-05-21 16:33:19 +0700

    test1

--- HotelReservation/src/java/hotel/controller/Facilities.java
+++ HotelReservation/src/java/hotel/controller/Facilities.java
@@ -1,5 +1,7 @@
 package hotel.controller;
 
+import hotel.helper.Fasilitas;
+import hotel.model.Admin;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -7,31 +9,47 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "Facilities", urlPatterns = {"/Facilities"})
 public class Facilities extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
+         Fasilitas fasilitas = new Fasilitas();
+         tampilkanDaftarFasilitas(fasilitas,request);
          request.getRequestDispatcher("index.jsp?page=facilities").forward(request, response);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
     }
-
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
     }
 
     @Override
     public String getServletInfo() {
         return "Short description";
     }
-
+    
+    private void tampilkanDaftarFasilitas( Fasilitas fasilitas, HttpServletRequest request)
+            throws SQLException {
+        List<Fasilitas> daftarFasilitas = fasilitas.lihatFasilitas();
+        request.setAttribute("daftarFasilitas", daftarFasilitas);
+    }
 }
