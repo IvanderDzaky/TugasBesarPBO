e84d8dc1e7d5ecb27ec3323a633a8e3f9c506aa7
Ivander Dzaky Khairullah <75924357+IvanderDzaky@users.noreply.github.com>
2025-05-25 22:05:58 +0700

    Update HotelReservation.sql

--- HotelReservation/src/java/hotel/sql/HotelReservation.sql
+++ HotelReservation/src/java/hotel/sql/HotelReservation.sql
@@ -20,24 +20,25 @@ SET time_zone = "+00:00";
 USE `hotelreservation`;
 
 -- --------------------------------------------------------
--- Struktur tabel: fasilitas
+-- Struktur tabel: users
 -- --------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fasilitas` (
-  `id_fasilitas` INT(11) NOT NULL AUTO_INCREMENT,
-  `nama_fasilitas` VARCHAR(100) NOT NULL UNIQUE,
-  `deskripsi_fasilitas` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_fasilitas`)
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` INT(11) NOT NULL AUTO_INCREMENT,
+  `nama` VARCHAR(255) DEFAULT NULL,
+  `email` VARCHAR(255) DEFAULT NULL UNIQUE,
+  `password` VARCHAR(255) DEFAULT NULL,
+  `isAdmin` TINYINT(4) NOT NULL DEFAULT 0,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Data fasilitas
-INSERT INTO `fasilitas` (`id_fasilitas`, `nama_fasilitas`, `deskripsi_fasilitas`) VALUES
-(1, 'WiFi', 'High speed WiFi quality with Indihome 500mb/s.'),
-(2, 'AC', 'Most coldest Air Conditioner that ever had.'),
-(3, 'TV', 'Smart Tv'),
-(4, 'Shower', 'Water Heater, Bathub'),
-(5, 'Cleaning', 'Cleaning Service'),
-(22, 'Coffe', 'Premium Coffe'),
-(25, 'Balcony', 'Premium Balcony');
+-- Data users
+INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `isAdmin`, `createdAt`) VALUES
+(23, 'mahmud', 'mahmud@gmail.com', '$2a$10$OzakO5ImwJ70GzUGh09TrufpiEs/mN0QmSzfxZj4E0s/kMjSA2YHS', 0, '2025-05-19 02:30:43'),
+(38, 'fattan', 'fattan@gmail.com', '$2a$10$Fn1HcptBuu3RlwMc08IO4eagqGylCrz72WAwg/PfgG62z1MR8hgky', 0, '2025-05-24 07:09:32'),
+(39, 'NIKO', 'Kayvon@gmail.com', '$2a$10$i5fc7WiJeMG4npJoMXeQWO5Sr/lWWhwNVf6aAMOIuMQ5R2h9ztwtq', 0, '2025-05-24 10:30:14'),
+(40, 'ADMIN', 'admin@yahoo.com', '$2a$10$.gefvWczpIRlk2bCW1BdYelovWNT1KsSbEqY5PdvUvDZZC7wQMVU6', 1, '2025-05-24 10:37:53');
 
 -- --------------------------------------------------------
 -- Struktur tabel: kamar
@@ -65,6 +66,26 @@ INSERT INTO `kamar` (`id_kamar`, `nomor_kamar`, `tipe`, `harga`, `status`, `max_
 (27, '501', 'VIP Suite', 350, 1, 15),
 (28, '502', 'VIP Suite', 350, 1, 15),
 (29, '601', 'Deluxe Suite', 400, 1, 20);
+
+-- --------------------------------------------------------
+-- Struktur tabel: fasilitas
+-- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fasilitas` (
+  `id_fasilitas` INT(11) NOT NULL AUTO_INCREMENT,
+  `nama_fasilitas` VARCHAR(100) NOT NULL UNIQUE,
+  `deskripsi_fasilitas` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id_fasilitas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data fasilitas
+INSERT INTO `fasilitas` (`id_fasilitas`, `nama_fasilitas`, `deskripsi_fasilitas`) VALUES
+(1, 'WiFi', 'High speed WiFi quality with Indihome 500mb/s.'),
+(2, 'AC', 'Most coldest Air Conditioner that ever had.'),
+(3, 'TV', 'Smart Tv'),
+(4, 'Shower', 'Water Heater, Bathub'),
+(5, 'Cleaning', 'Cleaning Service'),
+(22, 'Coffe', 'Premium Coffe'),
+(25, 'Balcony', 'Premium Balcony');
 
 -- --------------------------------------------------------
 -- Struktur tabel: kamar_fasilitas
@@ -127,27 +148,6 @@ CREATE TABLE IF NOT EXISTS `reservasi` (
 -- Data reservasi
 INSERT INTO `reservasi` (`id_reservasi`, `id_user`, `id_kamar`, `check_in`, `check_out`, `status`) VALUES
 (7, 3, 29, '2025-05-01', '2025-05-02', 'Dipesan');
-
--- --------------------------------------------------------
--- Struktur tabel: users
--- --------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` INT(11) NOT NULL AUTO_INCREMENT,
-  `nama` VARCHAR(255) DEFAULT NULL,
-  `email` VARCHAR(255) DEFAULT NULL UNIQUE,
-  `password` VARCHAR(255) DEFAULT NULL,
-  `isAdmin` TINYINT(4) NOT NULL DEFAULT 0,
-  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Data users
-INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `isAdmin`, `createdAt`) VALUES
-(23, 'mahmud', 'mahmud@gmail.com', '$2a$10$OzakO5ImwJ70GzUGh09TrufpiEs/mN0QmSzfxZj4E0s/kMjSA2YHS', 0, '2025-05-19 02:30:43'),
-(38, 'fattan', 'fattan@gmail.com', '$2a$10$Fn1HcptBuu3RlwMc08IO4eagqGylCrz72WAwg/PfgG62z1MR8hgky', 0, '2025-05-24 07:09:32'),
-(39, 'NIKO', 'Kayvon@gmail.com', '$2a$10$i5fc7WiJeMG4npJoMXeQWO5Sr/lWWhwNVf6aAMOIuMQ5R2h9ztwtq', 0, '2025-05-24 10:30:14'),
-(40, 'ADMIN', 'admin@yahoo.com', '$2a$10$.gefvWczpIRlk2bCW1BdYelovWNT1KsSbEqY5PdvUvDZZC7wQMVU6', 1, '2025-05-24 10:37:53');
-
 
 COMMIT;
 
